fd0e71ce0d8dedf11b2d2d7824ff73fbe6ac98be
kaceykooGithub <kacey.koo@gmail.com>
2024-04-20 11:36:55 +0100

    Update

--- java/net/kacey__/KaceysDatapack.java
+++ java/net/kacey__/KaceysDatapack.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 public class KaceysDatapack implements ModInitializer {
 	public static final String MOD_ID = "kaceysdatapack";
     public static final Logger LOGGER = LoggerFactory.getLogger("kaceysdatapack");
-
 	@Override
 	public void onInitialize() {
 		KaceysDatapack.LOGGER.info("Loading Kacey's Datapack...");
