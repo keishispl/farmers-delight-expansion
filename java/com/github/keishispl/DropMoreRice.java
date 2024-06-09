04b7097b91b879c802575fa22f2e05a8f5c4b2ee
kaceykooGithub <kacey.koo@gmail.com>
2024-06-09 08:39:28 +0100

    DropMoreRice

--- java/com/github/keishispl/DropMoreRice.java
+++ java/com/github/keishispl/DropMoreRice.java
@@ -5,11 +5,11 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class GetMoreRice implements ModInitializer {
+public class DropMoreRice implements ModInitializer {
 	public static final String MOD_ID = "getmorerice";
     public static final Logger LOGGER = LoggerFactory.getLogger("getmorerice");
 	@Override
 	public void onInitialize() {
-		GetMoreRice.LOGGER.info("Loading GetMoreRice...");
+		DropMoreRice.LOGGER.info("[DropMoreRice] Loading...");
 	}
 }
